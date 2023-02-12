f34d017caa72cd5d1ad53d59c78745977ea55ff3
SebastianG03 <guamandavid11235@gmail.com>
2023-02-12 11:05:53 -0500

    Backup of ma Scheduling task application with springboot

---
+++ src/main/java/scheduling_task_springboot/SchedulingTaskApplication.java
@@ -0,0 +1,13 @@
+package scheduling_task_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/scheduling_task_springboot/SchedulingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package scheduling_task_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
