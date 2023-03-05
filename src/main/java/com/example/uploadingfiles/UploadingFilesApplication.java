0fd8ecdecb62add742f9cfd4b3c45b1aea105327
SebastianG03 <guamandavid11235@gmail.com>
2023-03-05 14:41:28 -0500

    Update of file uploader spring boot app

--- src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
+++ src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -1,7 +1,10 @@
 package com.example.uploadingfiles;
 
+import com.example.uploadingfiles.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UploadingFilesApplication {
@@ -10,4 +13,11 @@ public class UploadingFilesApplication {
 		SpringApplication.run(UploadingFilesApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return ( args ) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
